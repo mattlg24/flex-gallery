afca590a3b66389e55afa0130a3d9d95ad1496f9
Matt Gardner <matthewlgardner@gmail.com>
2017-04-12 22:31:17 -0600

    add toggleActive function

--- app.js
+++ app.js
@@ -3,11 +3,14 @@ window.onload = function() {
     let panels = document.querySelectorAll('.panel')
 
     function toggleOpen() {
-      this.classList.toggle('open')
+        this.classList.toggle('open')
     }
 
     function toggleActive(e) {
-      console.log(e);
+        console.log(e.propertyName);
+        if (e.propertyName.includes('flex')) {
+            this.classList.toggle('open-active')
+        }
     }
 
     panels.forEach(panel => panel.addEventListener('click', toggleOpen))
