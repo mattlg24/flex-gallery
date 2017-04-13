b34c381ff912df237666ff1edb373346c8c2ddb0
Matt Gardner <matthewlgardner@gmail.com>
2017-04-12 22:45:32 -0600

    fix margin on body

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ window.onload = function() {
     }
 
     function toggleActive(e) {
-        console.log(e.propertyName);
         if (e.propertyName.includes('flex')) {
             this.classList.toggle('open-active')
         }
